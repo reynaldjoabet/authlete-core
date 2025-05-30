f0dba4fbf8c53a8876e1d6afe645d30ebf2322ff
Liyeuk Reynald <reynaldjoabet@gmail.com>
2025-05-31 00:30:17 +0400

    major refactoring

--- src/main/scala/models/package.scala
+++ src/main/scala/models/package.scala
@@ -77,6 +77,52 @@ package object models {
   given stringCodec: JsonValueCodec[String] =
     JsonCodecMaker.make(codecMakerConfig)
 
+  given unitCodec: JsonValueCodec[Unit] = new JsonValueCodec[Unit] {
+    override def decodeValue(in: JsonReader, default: Unit): Unit =
+      decode(in, 1024) // Max depth is 1024
+
+    override def encodeValue(x: Unit, out: JsonWriter): Unit = ()
+
+    override def nullValue: Unit = ()
+
+    private[this] def decode(in: JsonReader, depth: Int): Unit = {
+      val b = in.nextToken()
+      if (b == '"') {
+        in.rollbackToken()
+        val _ = in.readStringAsCharBuf()
+      } else if (b == 'f' || b == 't') {
+        in.rollbackToken()
+        val _ = in.readBoolean()
+      } else if ((b >= '0' && b <= '9') || b == '-') {
+        in.rollbackToken()
+        val _ = in.readFloat()
+      } else if (b == '[') {
+        val depthM1 = depth - 1
+        if (depthM1 < 0) in.decodeError("depth limit exceeded")
+        if (!in.isNextToken(']')) {
+          in.rollbackToken()
+          while ({
+            decode(in, depthM1)
+            in.isNextToken(',')
+          }) ()
+          if (!in.isCurrentToken(']')) in.arrayEndOrCommaError()
+        }
+      } else if (b == '{') {
+        val depthM1 = depth - 1
+        if (depthM1 < 0) in.decodeError("depth limit exceeded")
+        if (!in.isNextToken('}')) {
+          in.rollbackToken()
+          while ({
+            in.readKeyAsCharBuf()
+            decode(in, depthM1)
+            in.isNextToken(',')
+          }) ()
+          if (!in.isCurrentToken('}')) in.objectEndOrCommaError()
+        }
+      } else in.readNullOrError(nullValue, "expected JSON value")
+    }
+  }
+
 //implicit def arrayCodec[A: JsonValueCodec]: JsonValueCodec[Array[A]] = JsonCodecMaker.make(codecMakerConfig)
 
   implicit def listCodec[A: JsonValueCodec]: JsonValueCodec[List[A]] =
